f32cdbd56f8fe548bef715fac3aa4f100a606585
Maija Lintunokka <maija.lintunokka@naveocommerce.com>
2024-03-21 01:47:50 +0200

    fix: update framework version

--- src/routers/ExamplesRouter.ts
+++ src/routers/ExamplesRouter.ts
@@ -13,7 +13,7 @@ const router = new Router()
 router.get('/examples/headers', (ctx) => {
 	const headers = useHeaderParams(ctx, {
 		'x-basic-authentication': RequiredParam<{ username: string; password: string }>({
-			rehydrate: (v) => JSON.parse(v),
+			rehydrate: (v) => JSON.parse(v ?? ''),
 			validate: (v) => (v.username?.length ?? 0) > 0 && (v.password?.length ?? 0) > 0,
 			description: "Basic authentication header, includes 'username' and 'password'.",
 			errorMessage: "Must provide 'username' and 'password'.",
